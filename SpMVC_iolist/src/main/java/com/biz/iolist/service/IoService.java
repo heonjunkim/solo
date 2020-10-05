814cbc48a2a0c5d84aa0d643240aa6bca209c756
heonjunkim <maplegjswns@naver.com>
2020-10-05 11:48:28 +0900

    q

--- SpMVC_iolist/src/main/java/com/biz/iolist/service/IoService.java
+++ SpMVC_iolist/src/main/java/com/biz/iolist/service/IoService.java
@@ -5,7 +5,7 @@ import java.util.List;
 public interface IoService<VO, PK> {
 	
 	public List<VO> selectAll();
-	public VO findById(PK id);
+	public VO findByID(PK id);
 	public int insert(VO vo);
 	public int udpate(VO vo);
 	public int delete(VO vo);
